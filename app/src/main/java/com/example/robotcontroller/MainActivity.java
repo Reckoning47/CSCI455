4a8bb22c8cf19752e5a9c6ff7dd47c17665f2b79
ChrisCooper441 <day0fdiligence@gmail.com>
2020-02-26 15:17:33 -0700

    activity xml and Main

--- app/src/main/java/com/example/robotcontroller/MainActivity.java
+++ app/src/main/java/com/example/robotcontroller/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.robotcontroller;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-// Will need to add dependencies to git repo
-// TODO update from video https://montana.techsmithrelay.com/7Nq7
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText textEditableTalk;
+    Button buttonTalk;
+    TTS tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tts = new TTS(this);
+        tts.start();
+
+
+
+        textEditableTalk = (EditText) findViewById(R.id.textEditableTalk);
+        buttonTalk = (Button) findViewById(R.id.buttonTalk);
+        buttonTalk.setOnClickListener(this);
+
+
     }
+
+    public void onClick(View v) {
+        Toast.makeText(this, "onClick", Toast.LENGTH_SHORT).show();
+        switch(v.getId()) {
+            case R.id.buttonTalk:
+                String input = textEditableTalk.getText().toString();
+                Message sendMsg = tts.handler.obtainMessage();
+                Bundle b = new Bundle();
+                b.putString("TT", input);
+                sendMsg.setData(b);
+                tts.handler.sendMessage(sendMsg);
+                break;
+        }
+    }
+
+    public final Handler handler = new Handler() {
+        public void handleMessage(Message message) {
+            String m = message.getData().getString("result");
+        }
+    };
+
+
+
 }
+
+
