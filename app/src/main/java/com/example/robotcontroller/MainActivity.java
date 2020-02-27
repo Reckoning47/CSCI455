fba869dad9306fdafda910fe0d246bf0b68447d2
Jaret Boyer <jaret.c.boyer@gmail.com>
2020-02-26 18:24:55 -0700

    created client for tcp connection with raspberry pi

--- app/src/main/java/com/example/robotcontroller/MainActivity.java
+++ app/src/main/java/com/example/robotcontroller/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.robotcontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PORT = 9999;
+    private TCPClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button connectButton = findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String host = findViewById(R.id.hostInput).toString();
+                client = new TCPClient(host, PORT);
+                client.start();
+                //setContentView(R.layout.)
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/robotcontroller/TCPClient.java
@@ -0,0 +1,78 @@
+package com.example.robotcontroller;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.renderscript.ScriptGroup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.ref.WeakReference;
+import java.net.Socket;
+
+public class TCPClient extends Thread {
+
+    private static class TCPHandler extends Handler {
+
+        private final WeakReference<TCPClient> tcpClient;
+
+        public TCPHandler(TCPClient client) {
+            this.tcpClient = new WeakReference(client);
+        }
+
+        public void handleMessage(Message msg) {
+            TCPClient client = this.tcpClient.get();
+            String toSend = msg.getData().getString("TCP");
+            Message res = new Message();
+            res.obj = client.sendMessage(toSend);
+            client.handler.sendMessage(res);
+        }
+    }
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String host;
+    private int port;
+
+    public Handler handler;
+
+
+    TCPClient(String host, int port) {
+        super();
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        createConnection();
+        Looper.prepare();
+        this.handler = new TCPHandler(this);
+        Looper.loop();
+    }
+
+    public void createConnection() {
+        try {
+            this.socket = new Socket(this.host, this.port);
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String sendMessage(String msg) {
+        this.out.println(msg);
+        String response;
+        try {
+            response = this.in.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = null;
+        }
+        return response;
+    }
+}
