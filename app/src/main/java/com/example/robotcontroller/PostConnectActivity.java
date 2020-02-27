1357bd91ef4aa060143e642e3673a81cbee2c7e8
Jaret Boyer <jaret.c.boyer@gmail.com>
2020-02-26 18:51:39 -0700

    created post connection activity to do tts and buttons

---
+++ app/src/main/java/com/example/robotcontroller/PostConnectActivity.java
@@ -0,0 +1,14 @@
+package com.example.robotcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_connect);
+    }
+}
